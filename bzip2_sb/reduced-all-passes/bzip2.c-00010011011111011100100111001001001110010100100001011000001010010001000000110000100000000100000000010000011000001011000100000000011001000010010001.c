da4e92f7318cee330653e3b437b75083fa094a86
ugur-koc <ugurkoc.uk@hotmail.com>
2016-01-12 12:41:51 -0500

    removing old result

--- bzip2_sb/reduced-all-passes/bzip2.c-00010011011111011100100111001001001110010100100001011000001010010001000000110000100000000100000000010000011000001011000100000000011001000010010001.c
+++
@@ -1,7 +0,0 @@
-#ifndef _BZLIB_H
-typedef Int32;
-#endif
-generateMTFValues() {
-  Int32 zPend;
-  zPend = 0;
-}
--- bzip2_sb/reduced-all-passes/bzip2.c-10000101101010000001100110110010110100000011101000010000010000100001010001100111010110000000000100101001101010000011111000101010000001010010001110.c
+++
@@ -1,9 +0,0 @@
-#ifndef x0
-typedef x2;
-#endif
-x5() {
-  x2 zPend;
-  if (zPend > 0) {
-    zPend = 0;
-  }
-}
