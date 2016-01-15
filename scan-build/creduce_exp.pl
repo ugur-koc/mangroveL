862d646e71429a8908723cac8375aaa18d48459e
Ugur <ugurkoc.uk@hotmail.com>
2016-01-15 00:34:47 -0500

    modifying creduce
    modifying creduce

--- scan-build/creduce_exp.pl
+++ scan-build/creduce_exp.pl
@@ -9,10 +9,15 @@ use Math::BigFloat;
 my $program=$ARGV[0];
 my $tool=$ARGV[1]; #"cppcheck";
 my $interestingness=$ARGV[2];#"./test_interest.sh";
+my $id=Time::HiRes::time();
 
 my @passes = ();
 my $passesStr = "";
 my $runId = "";
+
+`mkdir runs/run-$id`;
+`cp $program runs/run-$id/$program`;
+`cp $interestingness runs/run-$id/$interestingness`;
 
 open ALL_PASSES,"<all_passes.txt" or die $!;
 while (my $line = <ALL_PASSES>){
@@ -26,10 +31,12 @@ while (my $line = <ALL_PASSES>){
    }else{      $runId=$runId."0"; }
 }
 
+$passesStr=~s/^\s+|\s+$//g;
+
 close ALL_PASSES;
-#print "$passesStr\n";
-`creduce --no-default-passes $passesStr $interestingness $program > log/runlog-$runId.txt`;
-if(-e "$program.orig") {
-   `mv $program reduced/$runId.c`;
-   `mv $program.orig $program`;
-}
+
+my $logFile="runlog.txt";
+#`cd runs/run-$id && echo "$runId" > runlog.txt 2>&1`;
+#`cd runs/run-$id && echo "$passesStr" >> runlog.txt 2>&1`;
+#--no-default-passes $passesStr
+`cd runs/run-$id && time creduce $interestingness $program >> runlog.txt 2>&1`;
--- scan-build/creduce_exp_run.sh
+++ scan-build/creduce_exp_run.sh
@@ -2,5 +2,5 @@
 
 for a in 1 2 3 4 5 6 7 8 9 10
 do
-   ./creduce_exp.pl CWE401_Memory_Leak__wchar_t_realloc_18.c cppcheck ./test_interest.sh
+   ./creduce_exp.pl sample.c scan-build ./test_interest.sh
 done
