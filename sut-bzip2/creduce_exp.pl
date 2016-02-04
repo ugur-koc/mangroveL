a9002834d5de0e8bc7e852fbe3ba319cd01cc401
ugur-koc <ugurkoc.uk@hotmail.com>
2016-02-03 19:08:26 -0500

    pass-lines off

--- sut-bzip2/creduce_exp.pl
+++ sut-bzip2/creduce_exp.pl
@@ -5,9 +5,8 @@ use 5.014;
 use warnings;
 use Time::HiRes;
 
-my $program=$ARGV[0];
-my $tool=$ARGV[1]; #"cppcheck";
-my $interestingness=$ARGV[2];#"./test_interest.sh";
+my $program=$ARGV[0]; # bzip2.c
+my $interestingness=$ARGV[1];#"./test_interest.sh";
 
 for (1..1) {
     my $id=Time::HiRes::time();
@@ -17,7 +16,7 @@ for (1..1) {
     `cp $program $runDir/$program`;
     `cp $interestingness $runDir/$interestingness`;
 
-    `cd $runDir && time creduce --save-temps --print-diff $interestingness $program >> runlog.txt 2>&1`;
+    `cd $runDir && time creduce $interestingness $program >> runlog.txt 2>&1`;
     `cd $runDir && rm -f $program.orig $interestingness`;
 
     `rm -fr /tmp/scan-build-2016-*`;
---
+++ sut-bzip2/runs/run-1454534603.33821/bzip2.c
@@ -0,0 +1,7 @@
+
+fn1
+#define BZ_STREAM_END 9
+    () {
+  int a;
+  a == BZ_STREAM_END;
+}
