b237f9939e9c1f2d8fa227959b6c2e4f813f4d01
ugur-koc <ugurkoc.uk@hotmail.com>
2016-10-22 20:50:40 -0400

    CWE122

---
+++ additional/run_sb_all.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use 5.014;
+use warnings;
+use Time::HiRes;
+
+my $workDir ="/home/ukoc/juliet/testcases";
+my $logDir="/home/ukoc/juliet-logs/";
+my @CWEFolders = ();
+
+sub run_sb($$){
+    my $runDir=shift;
+    my $logFile=shift;
+    `echo "Runing under $runDir" > $logFile`;
+    `cd $runDir && scan-build -disable-checker deadcode.DeadStores make >> $logFile 2>&1`;
+    `rm -fr /tmp/scan-build* >> $logFile 2>&1`;
+}
+
+opendir(DIR, $workDir) or die $!;
+while (my $folder = readdir(DIR)) {
+   if($folder eq ".." or $folder eq "."){ next; }
+   push(@CWEFolders, $folder);
+}
+
+foreach my $folder (@CWEFolders){
+   my $logfile=$logDir.substr($folder, 0, 6)."-scanbuild.log";
+   if (-d "$workDir/$folder/s01") {
+      opendir(DIR, "$workDir/$folder") or die $!;
+      while (my $subfolder = readdir(DIR)) {
+         if($subfolder eq ".." or $subfolder eq "." or (not (-e "$workDir/$folder/$subfolder/Makefile"))){ next; }
+         $logfile=$logDir.substr($folder, 0, 6).$subfolder."-scanbuild.log";
+	 run_sb("$workDir/$folder/$subfolder", $logfile);
+      }
+   } elsif (-e "$workDir/$folder/Makefile"){
+       run_sb("$workDir/$folder", $logfile);
+   }
+}
