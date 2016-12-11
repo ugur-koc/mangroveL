1be04747ef3c8abad6bf5f2164a32c5b6010b0c0
Ugur Koc <ugurkoc.uk@hotmail.com>
2016-12-10 22:55:40 -0500

    restructure repo
    removed large logs

--- sa-lstm/pickler.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin python
-
-import cPickle
-
-file_content = open("codeData/codeData.txt").read()
-with open('codeData/imdb.pkl', 'wb') as handle:
-  cPickle.dump(file_content, handle, -1)
--- sa-lstm/pklExt.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin python
-
-import pickle
-
-with open('data/imdb.pkl', 'rb') as f:
-    data = pickle.load(f)
-    print data
