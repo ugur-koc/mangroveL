1062d46924597e8640510a288d6dcfe66e7608e1
Ugur Koc <ugurkoc.uk@hotmail.com>
2016-12-12 15:32:43 -0500

    Juliet data set
    480 data points to used to test the model trained with owasp

--- sa-lstm/prepCWE.py
+++ sa-lstm/prepCWE.py
@@ -9,15 +9,15 @@ def get_immediate_subdirectories(a_dir):
 def obfuscate(sample):
 	randStr=''.join(random.choice(string.ascii_lowercase) for _ in xrange(3))
 	sample = re.sub('\s+', ' ', sample) # remove multi spaces
-	sample = re.sub('good', randStr, sample) #remove the word good 
-	sample = re.sub('bad', randStr, sample) #remove the word bad
+	#sample = re.sub('good', randStr, sample) #remove the word good 
+	#sample = re.sub('bad', randStr, sample) #remove the word bad
 	sample = re.sub('/\*[^;/]* \*/', '', sample) #remove most of the comments old pattern /\*[\w*:,\(\)-*\d\s_]* \*/
 	sample = re.sub('\"[\w+,\(\)\s]+\"', '" STRING "', sample) #unify print out strings
 	return sample.strip()
 
 def tokenize(sample):
 	helpDict={'\"':' " ', '\(':' ( ', '\)': ' ) ','\[':' [ ','\]': ' ] ','\{': ' { ', '\'':' \' ', ';':' ; ', '==':' == ', '<':' < ', '>':' > ', 
-	'!':' ! ', '\+\+':' ++ ', '--':' -- ', '\+': ' + ', '-':' - ', '\#':' # ', '\*':' * ', ',':' , ', '\!=':' != ', '&':' & ', '=':' = '} # '':'  '
+	'!':' ! ', '\+\+':' ++ ', '--':' -- ', '\+': ' + ', '-':' - ', '\#':' # ', '\*':' * ', ',':' , ', '\!=':' != ', '&':' & ', '=':' = ', '\.':' . '} # '':'  '
 	for key in helpDict:
 		sample = re.sub(key, helpDict[key], sample)
 	sample = re.sub('=\s=','==', sample)
@@ -25,42 +25,55 @@ def tokenize(sample):
 	sample = re.sub('<\s<','<<', sample)
 	sample = re.sub('/\s\*','/*', sample)
 	sample = re.sub('\*\s/','*/', sample)
-	sample = re.sub(' \d+ ',' N ', sample)
+	sample = re.sub(' \d{2-5} ',' N ', sample)
 	return sample.strip()
 
-def processSample(cweNumber, sampleClass, omitStr, lines, warningLine):
-	macroPattern = re.compile("#(if.*def|endif)")
+def processGood(lines, warningLine):
+	goodPattern = re.compile("void good\w*\(")
+	record=False
 	sample=''
-	isSampleLine=False
-	counter=1
-	sampleFirstLine=counter
+	counter=0
+	sampleFirstLine=0
 	for line in lines: 
-		if '/* '+omitStr+' */' in line:
-			break
-		elif omitStr in line:
-			isSampleLine = True
-			sampleFirstLine = counter+1
-		elif isSampleLine and not macroPattern.match(line):
+		counter=counter+1
+		if goodPattern.search(line):
+			sampleFirstLine=counter
+			record=True
 			sample+=line.rstrip()+' '
-		if counter+1==warningLine:
-			sample+='WARNINGLINE '
-		counter+=1
-	if isSampleLine and sample is not '' and counter > warningLine and sampleFirstLine < warningLine: 
-		print obfuscate(tokenize(sample)) + ' ' + cweNumber + ' ' + sampleClass + 'positive'
+		elif 'main(' in line:
+			break
+		elif record:
+			sample+=line.rstrip()+' '
+	print obfuscate(tokenize(sample)) + ' goodpositive'
 
-def processFile(fileName, warningLine):
-	cweNumber=fileName[fileName.rfind('/')+1::]
-	cweNumber=cweNumber[0:cweNumber.index('_')]
-	with open(fileName) as file: lines = file.readlines()
-	processSample(cweNumber, 'bad', 'OMITBAD', lines, warningLine)
-	processSample(cweNumber, 'good', 'OMITGOOD', lines, warningLine)
 
+def processBad(lines, warningLine):
+	goodPattern = re.compile("void good\w*\(")
+	badPattern = re.compile("void bad\w*\(")
+	record=False
+	sample=''
+	counter=0
+	sampleLastLine=0
+	for line in lines: 
+		counter=counter+1
+		if badPattern.search(line):
+			record=True
+			sample+=line.rstrip()+' '
+		elif goodPattern.search(line):
+			sampleLastLine=counter
+			break
+		elif record and '// ' not in line:
+			sample+=line.rstrip()+' '
+	print obfuscate(tokenize(sample)) +' badpositive'
+	
 if __name__ == '__main__':
-	pattern = re.compile("CWE\d{2,3}.*\.(c|cpp)")
-	julietDir = '/Users/ukoc/juliet/testcases/'
-	CWEs = 'CWE134_Uncontrolled_Format_String/s0*/'
-	with open('codeData/CWE134.txt') as file: lines = file.readlines()
+	warningFile='data/java/CWE89JulietStatsBalanced.txt'
+	julietDir = '/Users/ukoc/workspace/Juliet4J/mutants/'
+	with open(warningFile) as file: lines = file.readlines()
 	for line in lines:
-		(fileName,lineNumber,columnNumber,desc)=line.split(":")
-		for filepath in glob.glob(julietDir+CWEs+fileName):
-			processFile(filepath, int(lineNumber))
+		(fileName,lineNumber,desc,label)=line.split(",")
+		with open(julietDir+fileName[4:]) as file: lines = file.readlines()
+		if label == 'goodpositive':
+			processGood(lines, int(lineNumber))
+		else:
+			processBad(lines, int(lineNumber))
